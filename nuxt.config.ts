73c759b954a3da9026f5ac3bfc4e92afa0545197
Sn0wF1re <mokayaclinton34@gmail.com>
2025-12-31 12:16:45 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['@/assets/css/main.css'],
+  modules: [
+    '@nuxt/hints',
+    '@nuxt/image',
+    'motion-v/nuxt',
+    'shadcn-nuxt'
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui'
+  },
+})
